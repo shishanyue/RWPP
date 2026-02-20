2295597456d133f5ebc1c3d01f681a1e28c51c9a
JDSA Ling <CutelinXXX@126.com>
2026-02-21 01:48:14 +0800

    feat(contributors): 新增UnitConfig接口多个getValue系列调用方法体
    新增方法列表：
    - getValue
    - getAltValue
    - getValueAsBoolean
    - getValueAsLogicBooleanOrDefault
    - getValueAsPrimeBoolean
    - getValueAsStaticString
    - getValueAndRecordOrNull
    - getValueAndRecord
    - getValueAsMultiline
    - getValueAsUnit
    - getValueAsNumber
    - getValueAsLogicBoolean
    - getValueAsSpawnUnitArgs

--- rwpp-android/src/main/kotlin/io/github/rwpp/android/impl/utility/UnitConfig.kt
+++ rwpp-android/src/main/kotlin/io/github/rwpp/android/impl/utility/UnitConfig.kt
@@ -1,27 +1,104 @@
 package io.github.rwpp.android.impl.utility
 
+import io.github.rwpp.android.impl.LogicBooleanContextInternal
+import io.github.rwpp.android.impl.LogicBooleanInternal
+import io.github.rwpp.android.impl.LogicBooleanReturnTypeInternal
 import io.github.rwpp.android.impl.UnitConfigInternal
 import io.github.rwpp.android.impl.UnitTypeRefBaseInternal
+import io.github.rwpp.game.units.UnitRef
 import io.github.rwpp.game.units.custom.LogicMessage
 import io.github.rwpp.game.units.custom.UnitTypeRefBase
+import io.github.rwpp.game.units.custom.logicBooleans.LogicBoolean
 import io.github.rwpp.game.utility.UnitConfig
 import io.github.rwpp.inject.SetInterfaceOn
-import kotlin.String
+import io.github.rwpp.utils.Reflect
+
 
 @SetInterfaceOn([UnitConfigInternal::class])
 interface UnitConfigImpl: UnitConfig {
+
     val self: UnitConfigInternal
+
+    override fun getValue(sectionName: String?, key: String?, throwsException: Boolean): String?{
+        return Reflect.call<UnitConfigInternal,
+                String>(self,"b",
+            listOf(String::class,String::class, Boolean::class),
+            listOf(sectionName,key,throwsException))
+    }
+
+    override fun getAltValue(sectionName: String?, key1: String?, key2: String?): String?{
+        return Reflect.call<UnitConfigInternal,
+                String>(self,"g",
+            listOf(String::class,String::class,String::class),
+            listOf(sectionName,key1,key2))
+    }
 
     override fun getAltValueAsBoolean(sectionName: String?, key1: String?, key2: String?, defaultValue: Boolean?): Boolean?{
         return self.a(sectionName, key1, key2,defaultValue)
     }
 
-    override fun getValueAsLogicMessage(
-        unitTypeRefBase: UnitTypeRefBase?,
-        sectionName: String?,
-        key: String?,
-        defaultValue: String?
-    ): LogicMessage? {
+    override fun getValueAsBoolean(sectionName: String?, key: String?, defaultValue: Boolean?): Boolean?{
+        return self.a(sectionName,key,defaultValue)
+    }
+
+    override fun getValueAsLogicMessage(unitTypeRefBase: UnitTypeRefBase?, sectionName: String?, key: String?, defaultValue: String?): LogicMessage? {
         return self.a(unitTypeRefBase as UnitTypeRefBaseInternal, sectionName, key,defaultValue) as LogicMessage
     }
+
+    override fun getValueAsLogicBooleanOrDefault(unitTypeRefBase: UnitTypeRefBase?, sectionName: String?, key: String?, logicBoolean: LogicBoolean?
+    ): LogicBoolean?{
+        return self.a(unitTypeRefBase as UnitTypeRefBaseInternal,sectionName,key,logicBoolean as LogicBooleanInternal) as LogicBoolean
+    }
+
+    override fun getValueAsPrimeBoolean(sectionName: String?, key: String?): Boolean?{
+        return self.b(sectionName,key)
+    }
+
+    override fun getValueAsStaticString(sectionName: String?, key: String?, defaultValue: String?): String? {
+        return self.a(sectionName,key,defaultValue)
+    }
+
+   override fun getValueAndRecordOrNull(sectionName: String?, key: String?): String? {
+       return Reflect.call<UnitConfigInternal,
+               String>(self,"p",
+           listOf(String::class,String::class),
+           listOf(sectionName,key))
+    }
+
+    override fun getValueAndRecord(sectionName: String?, key: String?): String? {
+       return Reflect.call<UnitConfigInternal,
+               String>(self,"q",
+           listOf(String::class,String::class),
+           listOf(sectionName,key))
+    }
+
+    override fun getValueAsMultiline(str: String?): String? {
+      return Reflect.call<UnitConfigInternal,
+              String>(self,"e",
+          listOf(String::class),
+          listOf(str))
+    }
+
+    override fun getValueAsUnit(unitTypeRefBase: UnitTypeRefBase?, sectionName: String?, key: String?): LogicBoolean {
+        return self.b(unitTypeRefBase as UnitTypeRefBaseInternal,sectionName,key) as LogicBoolean
+    }
+
+    override fun getValueAsNumber(unitTypeRefBase: UnitTypeRefBase?, sectionName: String?, key: String?): LogicBoolean{
+        return self.c(unitTypeRefBase as UnitTypeRefBaseInternal,sectionName,key) as LogicBoolean
+    }
+
+    override fun getValueAsLogicBoolean(unitTypeRefBase: UnitTypeRefBase?, sectionName: String?, key: String?, returnType: LogicBoolean?): LogicBoolean{
+        return self.a(unitTypeRefBase as UnitTypeRefBaseInternal,sectionName,key,returnType as LogicBooleanInternal) as LogicBoolean
+    }
+
+    override fun getValueAsSpawnUnitArgs(str: String?, unitTypeRefBase: UnitTypeRefBase?, str2: String?, str3: String?): LogicBoolean{
+        return UnitConfigInternal.a(str,unitTypeRefBase as UnitTypeRefBaseInternal,str2,str3) as LogicBoolean
+    }
+
+    //TODO 无法编译
+    override fun getValueAsLogicBoolean(str: String?, unitTypeRefBase: UnitTypeRefBase?, str2: String?, str3: String?, returnType: LogicBooleanReturnTypeInternal?): LogicBoolean{
+        return UnitConfigInternal.a(str,unitTypeRefBase as UnitTypeRefBaseInternal,str2,str3,returnType as LogicBooleanReturnTypeInternal) as LogicBoolean
+    }
+
+
 }
--- rwpp-core-api/src/main/kotlin/io/github/rwpp/game/utility/UnitConfig.kt
+++ rwpp-core-api/src/main/kotlin/io/github/rwpp/game/utility/UnitConfig.kt
@@ -2,11 +2,44 @@ package io.github.rwpp.game.utility
 
 import io.github.rwpp.game.units.custom.LogicMessage
 import io.github.rwpp.game.units.custom.UnitTypeRefBase
-import kotlin.String
+import io.github.rwpp.game.units.custom.logicBooleans.LogicBoolean
+
 
 interface UnitConfig {
+
+
+    fun getValue(sectionName: String?, key: String?, throwsException: Boolean): String?
+
+    fun getAltValue(sectionName: String?, key1: String?, key2: String?): String?
+
     fun getAltValueAsBoolean(sectionName: String?, key1: String?, key2: String?, defaultValue: Boolean?): Boolean?
+
+    fun getValueAsBoolean(sectionName: String?, key: String?, defaultValue: Boolean?): Boolean?
 
     fun getValueAsLogicMessage(unitTypeRefBase: UnitTypeRefBase?, sectionName: String?,
                                key: String?, defaultValue: String?): LogicMessage?
+
+    fun getValueAsLogicBooleanOrDefault(unitTypeRefBase: UnitTypeRefBase?, sectionName: String?, key: String?, logicBoolean: LogicBoolean?
+    ): LogicBoolean?
+
+    fun getValueAsPrimeBoolean(sectionName: String?, key: String?): Boolean?
+
+    fun getValueAsStaticString(sectionName: String?, key: String?, defaultValue: String?): String?
+
+    fun getValueAndRecordOrNull(sectionName: String?, key: String?): String?
+
+    fun getValueAndRecord(sectionName: String?, key: String?): String?
+
+    fun getValueAsMultiline(str: String?): String?
+
+    fun getValueAsUnit(unitTypeRefBase: UnitTypeRefBase?, sectionName: String?, key: String?): LogicBoolean
+
+    fun getValueAsNumber(unitTypeRefBase: UnitTypeRefBase?, sectionName: String?, key: String?): LogicBoolean
+
+    fun getValueAsLogicBoolean(unitTypeRefBase: UnitTypeRefBase?, sectionName: String?, key: String?, returnType: LogicBoolean?): LogicBoolean
+
+    fun getValueAsSpawnUnitArgs(str: String?, unitTypeRefBase: UnitTypeRefBase?, str2: String?, str3: String?): LogicBoolean
+
+    fun getValueAsLogicBoolean(str: String?, unitTypeRefBase: UnitTypeRefBase?, str2: String?, str3: String?, returnType: LogicBoolean?): LogicBoolean?
+
 }
